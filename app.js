e7f08c2bfbf601f7d8f247d0b84dc91a98758911
xxholly32 <xxholly32@163.com>
2016-05-19 23:26:22 +0800

    [update]
    添加express 服务

--- app.js
+++ app.js
@@ -11,18 +11,20 @@ var app = express();
 
 //设置默认端口, 启动views
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, ''));
 
 app.engine('.html', ejs.__express);
-app.set('view engine', 'html');// app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '')));
 
-app.use('/', routes);
+app.use(function(req, res, next) {
+    res.render(req.originalUrl);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
